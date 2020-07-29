5d5d70547844583f4baefe88329510f78889f612
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-07-29 09:58:10 +0700

    Project Init and Creating Folder Structure

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+
+const path = require("path");
+
+module.exports = () => {
+  return {
+    entry: "./src/client/Index.js",
+    output: {
+      path: path.resolve(__dirname, "public"),
+      publicPath: "/",
+      filename: "bundle.js",
+    },
+    devServer: {
+      contentBase: "./",
+      publicPath: "/dist/",
+      historyApiFallback: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".json", ".wasm", ".mjs", "*"],
+    },
+  };
+};
