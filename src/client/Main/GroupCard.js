d9da70899eee3d57b6489eaa431b923248df4c7f
hieptien0512 <68257921+hieptien0512@users.noreply.github.com>
2020-07-29 16:33:25 +0700

    MainPage Implementation

---
+++ src/client/Main/GroupCard.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+export default class GroupCard extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-lg mt-5">
+          <Card style={{ width: "18rem", margin: "0 auto", float: "none", marginBottom: "10px" }}>
+            <Card.Img src="../../../public/BanhUot.png" />
+            <Card.Body style = {{padding : "0"}}>
+                <div style={{backgroundColor:"#FFE500"}}> 
+                    <Card.Title style ={{marginBottom:"0"}}>Group Name</Card.Title>
+                </div>
+              <Card.Text style={{backgroundColor:"#48BDFF"}}>
+                - Description <br></br>
+                - Description <br></br>
+                - Description
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-lg mt-5">
+          <Card style={{ width: "18rem", margin: "0 auto", float: "none", marginBottom: "10px", borderRadius:"25px" }}>
+            <Card.Img src="../../../public/BanhUot.png" />
+            <Card.Body style = {{padding : "0"}}>
+                <div style={{backgroundColor:"#FFE500"}}> 
+                    <Card.Title style ={{marginBottom:"0", padding:'5px'}}>Group Name</Card.Title>
+                </div>
+              <Card.Text style={{backgroundColor:"#48BDFF", borderBottomLeftRadius:"25px" , borderBottomRightRadius:"25px", padding: "10px" }}>
+                - Description <br></br>
+                - Description <br></br>
+                - Description
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-lg mt-5">
+          <Card style={{ width: "18rem", margin: "0 auto", float: "none", marginBottom: "10px" }}>
+            <Card.Img src="../../../public/BanhUot.png" />
+            <Card.Body style = {{padding : "0"}}>
+                <div style={{backgroundColor:"#FFE500"}}> 
+                    <Card.Title style ={{marginBottom:"0"}}>Group Name</Card.Title>
+                </div>
+              <Card.Text style={{backgroundColor:"#48BDFF"}}>
+                - Description <br></br>
+                - Description <br></br>
+                - Description
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-lg mt-5">
+          <Card style={{ width: "18rem", margin: "0 auto", float: "none", marginBottom: "10px" }}>
+            <Card.Img src="../../../public/BanhUot.png" />
+            <Card.Body style = {{padding : "0"}}>
+                <div style={{backgroundColor:"#FFE500"}}> 
+                    <Card.Title style ={{marginBottom:"0"}}>Group Name</Card.Title>
+                </div>
+              <Card.Text style={{backgroundColor:"#48BDFF"}}>
+                - Description <br></br>
+                - Description <br></br>
+                - Description
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
