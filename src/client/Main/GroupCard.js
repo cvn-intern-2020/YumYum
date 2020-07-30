73e5d00b512a18dc92e81f9f94e257f32764d096
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-07-30 10:26:40 +0700

    Update render method, fix eof

--- src/client/Main/GroupCard.js
+++ src/client/Main/GroupCard.js
@@ -4,68 +4,19 @@ import Button from "react-bootstrap/Button";
 export default class GroupCard extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-lg mt-5">
-          <Card className = "group-card">
-            <Card.Img src="../../../public/monan.png" />
-            <Card.Body style = {{padding : "0"}}>
-                <div style={{backgroundColor:"#FFE500"}}> 
-                    <Card.Title style ={{marginBottom:"0", padding:'5px'}}>Group Name</Card.Title>
-                </div>
-              <Card.Text className="group-cardtext">
-                - Description <br></br>
-                - Description <br></br>
-                - Description
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="col-lg mt-5">
-          <Card className = "group-card">
-            <Card.Img src="../../../public/monan.png" />
-            <Card.Body style = {{padding : "0"}}>
-                <div style={{backgroundColor:"#FFE500"}}> 
-                    <Card.Title style ={{marginBottom:"0", padding:'5px'}}>Group Name</Card.Title>
-                </div>
-              <Card.Text className="group-cardtext">
-                - Description <br></br>
-                - Description <br></br>
-                - Description
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="col-lg mt-5">
-          <Card className = "group-card">
-            <Card.Img src="../../../public/monan.png" />
-            <Card.Body style = {{padding : "0"}}>
-                <div style={{backgroundColor:"#FFE500"}}> 
-                    <Card.Title style ={{marginBottom:"0", padding:'5px'}}>Group Name</Card.Title>
-                </div>
-              <Card.Text className="group-cardtext">
-                - Description <br></br>
-                - Description <br></br>
-                - Description
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="col-lg mt-5">
-          <Card className = "group-card">
-            <Card.Img src="../../../public/monan.png" />
-            <Card.Body style = {{padding : "0"}}>
-                <div style={{backgroundColor:"#FFE500"}}> 
-                    <Card.Title style ={{marginBottom:"0", padding:'5px'}}>Group Name</Card.Title>
-                </div>
-              <Card.Text className="group-cardtext">
-                - Description <br></br>
-                - Description <br></br>
-                - Description
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
+      <Card className="group-card m-5" style={{display: "inline-block"}}>
+        <Card.Img src="../../../public/monan.png" />
+        <Card.Body style={{ padding: "0" }}>
+          <div style={{ backgroundColor: "#FFE500" }}>
+            <Card.Title style={{ marginBottom: "0", padding: "5px" }}>
+              Group Name
+            </Card.Title>
+          </div>
+          <Card.Text className="group-cardtext">
+            - Description <br></br>- Description <br></br>- Description
+          </Card.Text>
+        </Card.Body>
+      </Card>
     );
   }
 }
--- src/client/Main/MainBody.js
+++ src/client/Main/MainBody.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import GroupCard from "./GroupCard";
 export default class MainBody extends Component {
   render() {
+    let mock = [1, 2, 3, 4, 5, 6];
     return (
       <div
         style={{
@@ -11,7 +12,9 @@ export default class MainBody extends Component {
           height: "94%",
         }}
       >
-          <GroupCard></GroupCard>
+          {mock.map((mockValue) => (
+            <GroupCard key={mockValue}/>
+          ))}
       </div>
     );
   }
