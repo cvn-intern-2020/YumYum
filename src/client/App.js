d1b8324635a46902aa065f65f62c981ebc03a10f
hieptien0512 <68257921+hieptien0512@users.noreply.github.com>
2020-07-30 11:18:35 +0700

    Update signup page (#10)
    * Update signup page
    
    * Update Signup.js

--- src/client/App.js
+++ src/client/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Landing from "./Landing";
 import Main from "./Main";
 import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
 
 export default class App extends Component {
   constructor() {
@@ -15,6 +16,8 @@ export default class App extends Component {
           <Route exact path="/" render={(props) => <Landing {...props} />} />
           <Route exact path="/main" render={(props) => <Main {...props} />} />
           <Route exact path="/login" render={(props) => <Login {...props} />} />
+          <Route exact path="/signup" render={(props) => <Signup {...props} />} />
+
         </Switch>
       </>
     );
