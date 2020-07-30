780fdc7966d572677e68ed3fc0d89b5862e7a2aa
huy2512hcm <bahuyhoang2512@gmail.com>
2020-07-30 10:31:23 +0700

    Update Login.js

--- src/client/Auth/Login.js
+++ src/client/Auth/Login.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
-import BodyLogin from '../Login/BodyLogin';
-import NavbarLogin from '../Login/NavbarLogin';
+import React, { Component } from "react";
+import BodyLogin from "../Login/BodyLogin";
+import NavbarLogin from "../Login/NavbarLogin";
 
 export default class Login extends Component {
-    render() {
-        return <div className="h-100">
-        <NavbarLogin/>
-        <BodyLogin/>
-    </div>;
-        
-    }
+  render() {
+    return (
+      <div className="h-100">
+        <NavbarLogin />
+        <BodyLogin />
+      </div>
+    );
+  }
 }
