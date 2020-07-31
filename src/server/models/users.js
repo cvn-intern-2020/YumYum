c73d0d37401c120326b93a254c65a30fb11447ac
dcalau29 <68259267+dcalau29@users.noreply.github.com>
2020-07-31 09:00:40 +0700

    Update users.js

--- src/server/models/users.js
+++ src/server/models/users.js
@@ -5,14 +5,14 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 export const UsersSchema = new Users({
     name: { type: String, required: true },
-    phone: { type: Number, required: true},
-    email: { type: String, required: true},
-    password: { type: String, required: true},
-    groupId: [{type: ObjectId, required: true, ref: "Groups"}],
+    phone: { type: Number, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    groupId: [{ type: ObjectId, required: true, ref: "Groups" }],
 });
 
-UsersSchema.statics.getUserById = async function(userId){
-    let result = await this.findOne({_id: mongoose.Types.ObjectId(userId)});
+UsersSchema.statics.getUserById = async function (userId) {
+    let result = await this.findOne({ _id: mongoose.Types.ObjectId(userId) });
     console.log(result);
 }
 
