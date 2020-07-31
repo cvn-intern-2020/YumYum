8a34fe0fcbb8da1468e62a0e9647468d239c02bc
Nguyen Quang Lam <46278271+lamnguyencse17@users.noreply.github.com>
2020-07-31 09:06:59 +0700

    Update users.js

--- src/server/models/users.js
+++ src/server/models/users.js
@@ -8,17 +8,12 @@ export const UsersSchema = new Users({
     phone: { type: Number, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    groupId: [{ type: ObjectId, required: true, ref: "Groups" }],
+    groups: [{ type: ObjectId, required: true, ref: "Groups" }],
 });
 
 UsersSchema.statics.getUserById = async function (userId) {
     let result = await this.findOne({ _id: mongoose.Types.ObjectId(userId) });
-    console.log(result);
-  name: { type: String, required: true },
-  phone: { type: Number, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  groups: [{ type: ObjectId, required: true, ref: "Groups" }],
+    return result
 });
 
 UsersSchema.statics.createUser = async function (name, phone, email, password, groups) {
