6f64c1d300601f7b660dc74f0631e4770602adbd
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-07-31 10:43:51 +0700

    Login Backend Implemented and Routes Protection

--- src/server/routes/auth/signin.js
+++ src/server/routes/auth/signin.js
@@ -1,13 +1,38 @@
 import express from "express";
 import usersModel from "../../models/users";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
+  let user = await usersModel.getUserByEmail(email);
 
-  return res.status(200).json(await usersModel.getUserByEmail(email));
+  bcrypt.compare(password, user.password).then((isMatch) => {
+    if (isMatch) {
+      const payload = {
+        _id: user._id,
+        email: user.email,
+      };
+      jwt.sign(
+        payload,
+        "1234567890",
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          return res.status(200).json({
+            success: true,
+            token: "Bearer " + token,
+          });
+        }
+      );
+    } else {
+      return res.status(400).json({ message: "Password incorrect" });
+    }
+  });
 });
 
 module.exports = router;
