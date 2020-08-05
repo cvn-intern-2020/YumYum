38624dd8de05d005c17cd587937b0fa9b5881492
hieptien0512 <68257921+hieptien0512@users.noreply.github.com>
2020-08-05 11:18:00 +0700

    Schema adjust: Dish

--- src/server/models/dishes.js
+++ src/server/models/dishes.js
@@ -4,15 +4,26 @@ const Dishes = mongoose.Schema;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 export const DishesSchema = new Dishes({
-    groupId: {type: ObjectId, required: true, ref: "Groups"},
-    name: { type: String, required: true },
-    price: { type: Number, required: true}
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  deleteAt: { type: Boolean, required: true, default: null },
+  userId: { type: ObjectId, required: true, ref: "Users" },
 });
 
-DishesSchema.statics.getDishById = async function(dishId){
-    let result = await this.findOne({_id: mongoose.Types.ObjectId(dishId)}).select("-_id").lean();
-    return result;
+DishesSchema.statics.getDishById = async function (dishId) {
+  let result = await this.findOne({ _id: mongoose.Types.ObjectId(dishId) })
+    .select("-_id")
+    .lean();
+  return result;
 };
 
+DishesSchema.statics.createDish = async function (name, price, userId) {
+  let result = await this.create({
+    name: name,
+    price: price,
+    userId: mongoose.Types.ObjectId(userId),
+  });
+  return result;
+};
 const dishesModel = mongoose.model("Dishes", DishesSchema);
 export default dishesModel;
