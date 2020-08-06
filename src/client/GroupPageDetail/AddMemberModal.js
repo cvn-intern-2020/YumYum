3922597a180083a1c29991ea6037ab6ed70fd21d
hieptien0512 <68257921+hieptien0512@users.noreply.github.com>
2020-08-06 11:42:10 +0700

    Finish new member overlay (#33)
    * Finish new member overlay
    
    Co-authored-by: lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>

--- src/client/GroupPageDetail/AddMemberModal.js
+++ src/client/GroupPageDetail/AddMemberModal.js
@@ -1,20 +1,79 @@
 import React, { Component } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Form } from "react-bootstrap";
+import Validator from "validator";
+import Alert from "react-bootstrap/Alert";
 
 export default class AddMemberModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAlert: false,
+      err: "",
+      email: "",
+    };
+  }
+  toggleAlert = (err) => {
+    this.setState({ ...this.state, showAlert: !this.state.showAlert, err });
+  };
+
+  handleChange = (e) => {
+    this.setState({ ...this.state, [e.target.name]: e.target.value });
+  };
+  handleClickAddMember = () => {
+    if (this.state.email == "") {
+      this.toggleAlert("Empty fields");
+      return -1;
+    }
+    if (!Validator.isEmail(this.state.email)) {
+      this.toggleAlert("Invalid email");
+      return -1;
+    }
+    console.log(this.state.email);
+    this.props.handleClose();
+  };
   render() {
     return (
       <Modal show={this.props.show} onHide={this.props.handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Add New Member</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          {this.state.showAlert ? (
+            <Alert variant={"danger"} onClose={this.toggleAlert} dismissible>
+              {this.state.err}
+            </Alert>
+          ) : (
+            <></>
+          )}
+          <Form.Group>
+            <Form.Label>Member Email: </Form.Label>
+            <Form.Control
+              size="lg"
+              type="email"
+              name="email"
+              placeholder="abcdxy@example.com"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={this.props.handleClose}>
+          <Button
+            style={{
+              backgroundColor: "#FF5522",
+              color: "#080024",
+              border: "none",
+            }}
+            variant="secondary"
+            onClick={this.props.handleClose}
+          >
             Close
           </Button>
-          <Button variant="primary" onClick={this.props.handleClose}>
-            Save Changes
+          <Button
+            className="landing-button"
+            variant="primary"
+            onClick={this.handleClickAddMember}
+          >
+            Add Member
           </Button>
         </Modal.Footer>
       </Modal>
