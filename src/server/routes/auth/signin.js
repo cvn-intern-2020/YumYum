2e03566dd593ce33dca4b209826b9e9c877215e3
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-06 13:20:39 +0700

    Fix user does not exist

--- src/server/routes/auth/signin.js
+++ src/server/routes/auth/signin.js
@@ -9,6 +9,9 @@ router.post("/", async (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
   let user = await usersModel.getUserByEmail(email);
+  if (!user) {
+    return res.status(400).json({ message: "User does not exist" });
+  }
 
   bcrypt.compare(password, user.password).then((isMatch) => {
     if (isMatch) {
