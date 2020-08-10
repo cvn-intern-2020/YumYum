6b038a7a2b6943a5d76bd6d12e9ba15ff0df93b5
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-10 08:54:11 +0700

    Refactor MVC pattern-signIn

--- src/server/controllers/Auth.js
+++ src/server/controllers/Auth.js
@@ -1,5 +1,6 @@
 import { getUserByEmail } from "../services/userService";
 import { signUpService } from "../services/authService";
+import passwordCompare from "../utils/passwordCompare";
 
 export const signUpController = async (req, res) => {
   let { name, phone, email, password } = req.body;
@@ -12,4 +13,21 @@ export const signUpController = async (req, res) => {
     return res.status(200).json(result);
   }
   return res.status(400).json({ message: "Something went wrong" });
+};
+
+export const signInController = async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let user = await getUserByEmail(email);
+  if (!user.status) {
+    return res.status(400).json({ message: user.message });
+  }
+  user = user.result;
+  let result = await passwordCompare(password, user);
+  if (result.status) {
+    return res.status(200).json({
+      token: result.token,
+    });
+  }
+  return res.status(400).json(result.message);
 };
