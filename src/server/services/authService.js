c786229ee29fd767418cb6c5c8b46a6cef483fef
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-10 10:06:15 +0700

    fix wrong argument passed

--- src/server/services/authService.js
+++ src/server/services/authService.js
@@ -1,7 +1,7 @@
 import { createUser } from "./userService";
 import passwordHash from "../utils/passwordHash";
 
-export const signUpService = async (email, phone, name, password) => {
+export const signUpService = async (email, phone, password, name) => {
   let result = await createUser(email, phone, passwordHash(password), name);
   return result;
 };
