3cf44dd484d4d90b7f6a2bb319d20570f61a5232
huy2512hcm <bahuyhoang2512@gmail.com>
2020-08-12 14:52:46 +0700

    Add (+-) Interaction Buttons

--- src/client/components/GroupPageDetail/DishItem.js
+++ src/client/components/GroupPageDetail/DishItem.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import { Button } from "react-bootstrap";
 export default class DishItem extends Component {
   render() {
     return (
       <ListGroup.Item>
         <div className="row w-100 m-0">
-    <div className=" dish-label col">{this.props.dish.name} </div>
-          <div className="dish-label col "></div>
+          <div className=" dish-label col">{this.props.dish.name} </div>
+          <div style = {{fontSize: "1.6rem"}}className="dish-label-quantity col ">
+            <Button
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                color: "#080024",
+                fontSize: "1.6rem",
+                paddingTop: 0,
+                boxShadow: "none"
+              }}
+            >
+              [-]
+            </Button>
+            1
+            <Button
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                color: "#080024",
+                fontSize: "1.6rem",
+                paddingTop: 0,
+                boxShadow: "none"
+              }}
+            >
+              [+]
+            </Button>
+          </div>
           <div className="dish-label col ">{this.props.dish.price}</div>
           <div className="dish-label col"></div>
         </div>
