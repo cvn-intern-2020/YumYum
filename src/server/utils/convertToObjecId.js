f900edc3da3001ed3f521857d6a700bced5ad950
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-13 16:41:56 +0700

    Update convertToObjecId.js

--- src/server/utils/convertToObjecId.js
+++ src/server/utils/convertToObjecId.js
@@ -3,9 +3,8 @@ import mongoose from "mongoose";
 const convertToObjectId = (source) => {
   for (let detail of source) {
     detail.dishId = mongoose.Types.ObjectId(detail._id);
-    delete detail._id
+    delete detail._id;
   }
-  console.log(source);
   return source;
 };
 
