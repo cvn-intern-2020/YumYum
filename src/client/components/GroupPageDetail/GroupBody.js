2d13ab3b65a7865f62faf6b3d564d01879c23f8e
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-13 13:48:15 +0700

    Move Group Body Button Bar

--- src/client/components/GroupPageDetail/GroupBody.js
+++ src/client/components/GroupPageDetail/GroupBody.js
@@ -9,6 +9,7 @@ import { getGroupRequest, editDishesInGroupRequest } from "../../request/group";
 import DishList from "./DishListUser";
 import DishListUser from "./DishListUser";
 import OrderConfirmModal from "./OrderConfirmModal";
+import ButtonBar from "./ButtonBar";
 
 class GroupBody extends Component {
   constructor(props) {
@@ -17,7 +18,7 @@ class GroupBody extends Component {
       showAddMemberModal: false,
       showConfirmOrderModal: false,
       dishes: [], // id price quantity sum
-      totalPrice: 0
+      totalPrice: 0,
     };
   }
   toggleAddMemberModal = () => {
@@ -37,10 +38,13 @@ class GroupBody extends Component {
       }
       return dish;
     });
-    this.setState({
-      ...this.state,
-      dishes: [...newDishState],
-    }, () => this.calculateTotalPrice());
+    this.setState(
+      {
+        ...this.state,
+        dishes: [...newDishState],
+      },
+      () => this.calculateTotalPrice()
+    );
   };
 
   calculateTotalPrice = () => {
@@ -49,7 +53,7 @@ class GroupBody extends Component {
       totalDishesCalculate += dish.quantity * dish.price;
     });
     this.setState({ ...this.state, totalPrice: totalDishesCalculate });
-  }
+  };
 
   toggleConfirmOrderModal = () => {
     this.setState({
@@ -98,34 +102,8 @@ class GroupBody extends Component {
           handleClose={this.toggleConfirmOrderModal}
           {...this.props}
         />
-        <div className="row w-100 m-0">
-          <div className="col-4">
-            <Button
-              style={{ backgroundColor: "#FF5522", color: "#080024" }}
-              className="float-left ml-5 mt-4 group-button"
-              onClick={this.toggleAddMemberModal}
-            >
-              Add Member
-            </Button>{" "}
-          </div>
 
-          <div
-            className="mt-4 col-4"
-            style={{ fontSize: "40px", textAlign: "center", color: "white" }}
-          >
-            {this.state.name}
-          </div>
-
-          <div className="col-4">
-            <Button
-              style={{ backgroundColor: "#48BDFF", color: "#080024" }}
-              className="float-right mt-4 mr-5 group-button"
-              onClick={editDishesInGroupRequest}
-            >
-              Edit Dishes
-            </Button>
-          </div>
-        </div>
+        <ButtonBar toggleAddMemberModal={this.toggleAddMemberModal} name={this.state.name}/>
 
         <DishListUser
           changeDishAmount={this.changeDishAmount}
