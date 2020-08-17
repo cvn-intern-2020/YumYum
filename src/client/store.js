f41172f5b8893e4a68a53dfc060b7fd7d95cebac
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-17 16:51:25 +0700

    remove redux dev tool

--- src/client/store.js
+++ src/client/store.js
@@ -1,20 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import * as actionCreators from "./actions/types";
 
-const initialState = {};
-const composeEnhancers = composeWithDevTools({
-  actionCreators,
-  trace: true,
-  traceLimit: 25,
-});
 const middleWare = [thunk];
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(...middleWare))
-);
+const store = createStore(rootReducer, applyMiddleware(...middleWare));
 
 export default store;
